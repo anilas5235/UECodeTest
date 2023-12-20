8b6c96e7f343b1b75d2066f7d8e81edcd787b4a3
anilas5235 <niklas-borchers@outlook.de>
2023-12-20 23:01:14 +0100

    Initial commit

---
+++ Source/CodeTest/CodeTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CodeTest : ModuleRules
+{
+	public CodeTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
