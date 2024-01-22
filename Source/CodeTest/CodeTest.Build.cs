0c40129e8400e4197e3bde6bfd3b5ac39b1ce815
anilas5235 <niklas-borchers@outlook.de>
2024-01-22 22:41:13 +0100

    cpp Widget the begin of the end

--- Source/CodeTest/CodeTest.Build.cs
+++ Source/CodeTest/CodeTest.Build.cs
@@ -8,6 +8,9 @@ public class CodeTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","Niagara","UMG","Slate"
+		});
 	}
 }
